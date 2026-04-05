c9d3ced34d55ad3d633cb2208fc770cbe0e2f433
IMeeus <maeleeuxs@gmail.com>
2026-04-05 15:33:59 +0200

    particles

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rb;
     private Animator _animator;
 
-    public InputActionReference jumpAction;
     public float jumpForce = 10;
     public float gravityModifier = 1;
     public bool isOnGround = true;
     public bool isGameOver = false;
+
+    public InputActionReference jumpAction;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
 
     void Start()
     {
@@ -32,6 +35,7 @@ public class PlayerController : MonoBehaviour
             _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             _animator.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
 
@@ -40,6 +44,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
 
         if (collision.gameObject.CompareTag("Obstacle"))
@@ -47,6 +52,8 @@ public class PlayerController : MonoBehaviour
             isGameOver = true;
             _animator.SetBool("Death_b", true);
             _animator.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
 }

aaef96538dcf8e75b41d56c879939d458840bb29
IMeeus <maeleeuxs@gmail.com>
2026-04-05 19:13:36 +0200

    setup movement

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private GameObject _focalPoint;
+
+    public float moveSpeed = 5;
+
+    public InputActionReference moveAction;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        var verticalInput = moveAction.action.ReadValue<Vector2>().y;
+        var moveDirection = _focalPoint.transform.forward;
+
+        _rb.AddForce(verticalInput * moveSpeed * moveDirection);
+    }
+}
--- Prototype4/Assets/Scripts/RotateCamera.cs
+++ Prototype4/Assets/Scripts/RotateCamera.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RotateCamera : MonoBehaviour
 {
+    public float rotationSpeed = 100;
+    public InputActionReference moveAction;
 
-    // Update is called once per frame
     void Update()
     {
-
+        var moveInput = moveAction.action.ReadValue<Vector2>();
+        transform.Rotate(Vector3.down, moveInput.x * rotationSpeed * Time.deltaTime);
     }
 }

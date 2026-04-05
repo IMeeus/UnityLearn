c72c4b4ea60ce2bef8f4b4241f6a2dc1c1163d32
IMeeus <maeleeuxs@gmail.com>
2026-04-05 18:51:27 +0200

    Finish challenge 3, setup p4

--- Prototype3/Assets/Challenge 3/Scripts/SpawnManagerX.cs
+++ Prototype3/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -16,7 +16,7 @@ public class SpawnManagerX : MonoBehaviour
 
     void SpawnObjects()
     {
-        Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+        Vector3 spawnLocation = new(30, Random.Range(5, 15), 0);
         int index = Random.Range(0, objectPrefabs.Length);
 
         if (!playerControllerScript.gameOver)
